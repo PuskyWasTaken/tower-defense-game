4e68fe08f0e741ae87a675dc498e1b2480ca322b
SandraTazlaoanu <sandra.tazlaoanu79@gmail.com>
2018-12-08 13:44:31 +0200

    Added configurations to the test project.

---
+++ TowerDefenceGame/Tests/EntityTest.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include"SFML/Graphics.hpp"
+#include"../TowerDefenceGame/Entity.h"
+#include"../TowerDefenceGame/Globals.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Tests
+{		
+	TEST_CLASS(EntityTest)
+	{
+	public:
+		
+		TEST_METHOD(DefaultConstructor)
+		{
+			
+		}
+
+	};
+
+}
